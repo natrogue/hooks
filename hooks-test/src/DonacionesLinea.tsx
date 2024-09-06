63a76e6501244c7d7b0872d92f16fb7feedc32ca
natrogue28 <A01663909@tec.mx>
2024-09-06 15:59:27 -0600

    connection w db successfull

--- hooks-test/src/DonacionesLinea.tsx
+++ hooks-test/src/DonacionesLinea.tsx
@@ -1,6 +1,7 @@
 import { useMediaQuery, Theme } from '@mui/material';
 import { List, Datagrid, TextField, SimpleList, Create, TextInput, SimpleForm, NumberInput, required } from 'react-admin';
 
+// Componente para listar donaciones en línea
 export const DonacionesLineaList = () => {
     const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down('sm'));
     return (
@@ -13,25 +14,25 @@ export const DonacionesLineaList = () => {
                 />
             ) : (
                 <Datagrid>
-                    <TextField source="ID" />
-                    <TextField source="Donor Name" />
-                    <TextField source="Amount" />
-                    <TextField source="Date" />
-                    <TextField source="Section" />
-                    
-
+                    <TextField source="id" label="ID" />  {/* Mapea el id desde _id */}
+                    <TextField source="donorName" label="Donor Name" />  {/* Nombre correcto del campo */}
+                    <TextField source="amount" label="Amount" />  {/* Nombre correcto del campo */}
+                    <TextField source="date" label="Date" />  {/* Nombre correcto del campo */}
+                    <TextField source="section" label="Section" />  {/* Nombre correcto del campo */}
                 </Datagrid>
             )}
         </List>
     );
 };
 
+// Componente para crear una donación en línea
 export const DonacionesLineaCreate = () => (
     <Create>
         <SimpleForm>
-            <TextInput source="Donor Name" validate={[required()]} />
-            <NumberInput source="Amount" validate={[required()]} />
-            <TextInput source="Date" />
+            <TextInput source="donorName" label="Donor Name" validate={[required()]} />  {/* Nombre correcto del campo */}
+            <NumberInput source="amount" label="Amount" validate={[required()]} />  {/* Nombre correcto del campo */}
+            <TextInput source="date" label="Date" />  {/* Nombre correcto del campo */}
+            <TextInput source="section" label="Section" />  {/* Añadido el campo 'Section' */}
         </SimpleForm>
     </Create>
 );
