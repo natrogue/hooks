da5f6ae27a2e2c9de7e64dc4c0a480e1d513ffc1
natrogue28 <A01663909@tec.mx>
2024-09-18 17:39:31 -0600

    fixed donaciones especie

--- hooks-test/src/DonacionesEspecie.tsx
+++ hooks-test/src/DonacionesEspecie.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import { useMediaQuery, Theme } from '@mui/material';
 import { List, Datagrid, TextField, SimpleList, Create, 
-    TextInput, SimpleForm, SelectInput, required } from 'react-admin';
-import IfCanAccess from 'react-admin';
-
-const donationTypes = [
-    { id: 'campaña', name: 'Campaña' },
-    { id: 'patrocina', name: 'Patrocina' }
-];
+    TextInput, SimpleForm, NumberInput, required } from 'react-admin';
 
 export const DonacionesEspecieList = () => {
     const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down('sm'));
@@ -16,16 +10,16 @@ export const DonacionesEspecieList = () => {
             {isSmall ? (
                 <SimpleList
                     primaryText={record => record.donorName}
-                    secondaryText={record => record.item}
-                    tertiaryText={record => record.donationType}
+                    secondaryText={record => `$${record.amount}`}
+                    tertiaryText={record => new Date(record.date).toLocaleDateString()}
                 />
             ) : (
                 <Datagrid>
                     <TextField source="id" label="ID" />
-                    <TextField source="donorName" label="Donor Name" />
-                    <TextField source="item" label="Item" />
-                    <TextField source="donationType" label="Donation Type" />
-                    <TextField source="comment" label="Comment" />
+                    <TextField source="donorName" label="Nombre" />
+                    <TextField source="amount" label="Cantidad" />
+                    <TextField source="date" label="Fecha" />
+                    <TextField source="event" label="Evento" /> {/* Changed 'event' instead of 'section' */}
                 </Datagrid>
             )}
         </List>
@@ -35,11 +29,10 @@ export const DonacionesEspecieList = () => {
 export const DonacionesEspecieCreate = () => (
     <Create>
         <SimpleForm>
-            <TextInput source="donorName" label="Donor Name" validate={[required()]} />
-            <TextInput source="item" label="Item" validate={[required()]} />
-            <SelectInput source="donationType" label="Donation Type" choices={donationTypes} />
-            <TextInput source="comment" label="Comment" />
+            <TextInput source="donorName" label="Nombre" validate={[required()]} />
+            <NumberInput source="amount" label="Cantidad" validate={[required()]} /> {/* Changed 'item' to 'amount' */}
+            <TextInput source="date" label="Fecha" />  {/* Date field */}
+            <TextInput source="event" label="Evento" />  {/* New 'event' field */}
         </SimpleForm>
     </Create>
 );
-
