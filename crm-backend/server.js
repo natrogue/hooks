062801dfb7a1897004a060bbe90fc0f576ef46c5
natrogue28 <A01663909@tec.mx>
2024-09-18 16:38:58 -0600

    fixed warning

--- crm-backend/server.js
+++ crm-backend/server.js
@@ -21,8 +21,6 @@ app.use(express.json());
 
 // Conectar a la base de datos MongoDB
 mongoose.connect('mongodb://localhost:27017/crmDummy', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
 }).then(() => {
     console.log('Conectado a la base de datos MongoDB');
 }).catch((err) => {
